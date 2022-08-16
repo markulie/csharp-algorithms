4caef6998dcfd1d00c869d6dcb0869ecd5676f15
markhuna <markhuna@gmail.com>
2022-08-16 10:32:09 +0400

    u

---
+++ GetAverageDiveded5/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] arr = { 3, 25, 6, 10, 99 };
+
+int sum = 0;
+for (int i = 0; i < arr.Length; i++) if (arr[i] % 5 == 0) sum += arr[i];
+int average = sum / arr.Length;
+
+Console.WriteLine(average);
+
+Console.ReadLine();
