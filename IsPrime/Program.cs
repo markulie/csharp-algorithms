c3f1ab52af8abdc084a86dcdcd8b5bba0b5801d6
Mark <markhuna@gmail.com>
2023-05-30 17:22:42 +0400

    u

--- IsPrime/Program.cs
+++ IsPrime/Program.cs
@@ -1,33 +1,40 @@
-﻿static int GetPositiveIntegerFromUser()
-{
-    while (true)
-    {
-        Console.Write("Enter a positive integer: ");
-        string? input = Console.ReadLine();
-        if (int.TryParse(input, out int number) && number > 0) return number;
-        Console.WriteLine("ERROR: Invalid input. Please enter a positive integer.");
-    }
-}
+﻿namespace IsPrime;
 
-static bool CheckPrimeNumber(int number)
+internal abstract class Program
 {
-    if (number == 2) return true;
-    else if (number < 2 || number % 2 == 0) return false;
-    else
+    public static void Main()
     {
-        double sqrtNumber = Math.Sqrt(number);
-        for (int i = 3; i <= sqrtNumber; i += 2)
+        while (true)
         {
-            if (number % i == 0) return false;
+            int number = GetPositiveIntegerFromUser();
+            bool isPrime = IsPrime(number);
+            Console.WriteLine($"{number} is {(isPrime ? "" : "NOT ")}a prime number. ");
         }
     }
 
-    return true;
-}
+    private static int GetPositiveIntegerFromUser()
+    {
+        while (true)
+        {
+            Console.Write("Enter a positive integer: ");
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int number) && number > 0) return number;
+            Console.WriteLine("ERROR: Invalid input. Please enter a positive integer.");
+        }
+    }
 
-while (true)
-{
-    int number = GetPositiveIntegerFromUser();
-    bool isPrime = CheckPrimeNumber(number);
-    Console.WriteLine($"{number} is {(isPrime ? "" : "NOT ")}a prime number. ");
+    private static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+        if (number is 2 or 3) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
+
+        int sqrtNumber = (int)Math.Sqrt(number);
+        for (int i = 5; i <= sqrtNumber; i += 6)
+        {
+            if (number % i == 0 || number % (i + 2) == 0) return false;
+        }
+
+        return true;
+    }
 }
