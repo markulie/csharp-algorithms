b246ac024f2c704b0ff7a25f68566629f0dda064
Mark <markhuna@gmail.com>
2023-06-05 13:40:47 +0400

    u

---
+++ FindIndex/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] array = new int[100];
+
+for (int i = 0; i < 99; i++)
+{
+    array[i] = 0;
+}
+
+Random random = new();
+int randomIndex = random.Next(1, 100);
+array[randomIndex] = 1;
+
+foreach (int i in array)
+{
+    if (i == 1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(1);
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.Write(i);
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(array.Length);
+
+(int, int) index2 = MyMethod(array);
+Console.WriteLine("Index: " + index2.Item1 + ", Attempts: " + index2.Item2);
+
+static (int, int) MyMethod(int[] x)
+{
+    int attempts = 0;
+    int length = x.Length;
+    int half = length / 2;
+
+    for (int i = half; i < length; i++)
+    {
+        attempts++;
+        if (x[i] == 1) return (i, attempts);
+    }
+
+    for (int i = half - 1; i > 0; i--)
+    {
+        attempts++;
+        if (x[i] == 1) return (i, attempts);
+    }
+
+    return (-1, attempts);
+}
--- IsPrime/Program.cs
+++ IsPrime/Program.cs
@@ -1,40 +1,32 @@
-﻿namespace IsPrime;
-
-internal abstract class Program
+﻿while (true)
 {
-    public static void Main()
+    int number = GetPositiveIntegerFromUser();
+    bool isPrime = IsPrime(number);
+    Console.WriteLine($"{number} is {(isPrime ? "" : "NOT ")}a prime number. ");
+}
+
+static int GetPositiveIntegerFromUser()
+{
+    while (true)
     {
-        while (true)
-        {
-            int number = GetPositiveIntegerFromUser();
-            bool isPrime = IsPrime(number);
-            Console.WriteLine($"{number} is {(isPrime ? "" : "NOT ")}a prime number. ");
-        }
+        Console.Write("Enter a positive integer: ");
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out int number) && number > 0) return number;
+        Console.WriteLine("ERROR: Invalid input. Please enter a positive integer.");
+    }
+}
+
+static bool IsPrime(int number)
+{
+    if (number < 2) return false;
+    if (number is 2 or 3) return true;
+    if (number % 2 == 0 || number % 3 == 0) return false;
+
+    int sqrtNumber = (int)Math.Sqrt(number);
+    for (int i = 5; i <= sqrtNumber; i += 6)
+    {
+        if (number % i == 0 || number % (i + 2) == 0) return false;
     }
 
-    private static int GetPositiveIntegerFromUser()
-    {
-        while (true)
-        {
-            Console.Write("Enter a positive integer: ");
-            string? input = Console.ReadLine();
-            if (int.TryParse(input, out int number) && number > 0) return number;
-            Console.WriteLine("ERROR: Invalid input. Please enter a positive integer.");
-        }
-    }
-
-    private static bool IsPrime(int number)
-    {
-        if (number < 2) return false;
-        if (number is 2 or 3) return true;
-        if (number % 2 == 0 || number % 3 == 0) return false;
-
-        int sqrtNumber = (int)Math.Sqrt(number);
-        for (int i = 5; i <= sqrtNumber; i += 6)
-        {
-            if (number % i == 0 || number % (i + 2) == 0) return false;
-        }
-
-        return true;
-    }
+    return true;
 }
