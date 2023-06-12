62bee226b0cf769cfa8e71572a0d31c56b412c4a
Mark <markhuna@gmail.com>
2023-06-13 01:47:29 +0400

    u

---
+++ StringVStringBuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+string s = "s";
+StringBuilder b = new("b");
+const int max = 1000;
+
+//---------------------------------
+
+Stopwatch stopwatchS = new();
+stopwatchS.Start();
+for (int i = 0; i < max; i++)
+{
+    s += i;
+    Console.WriteLine(s);
+}
+stopwatchS.Stop();
+TimeSpan elapsedTimeS = stopwatchS.Elapsed;
+
+//---------------------------------
+
+Stopwatch stopwatchB = new();
+stopwatchB.Start();
+for (int i = 0; i < max; i++)
+{
+    b.Append(i);
+    Console.WriteLine(b);
+}
+stopwatchB.Stop();
+TimeSpan elapsedTimeB = stopwatchB.Elapsed;
+
+//---------------------------------
+
+Console.WriteLine($"s: {elapsedTimeS}");
+Console.WriteLine($"b: {elapsedTimeB}");
