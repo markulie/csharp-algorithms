7e0b178200e26026811373b6fcee2bd73f7a0966
Mark <markhuna@gmail.com>
2023-06-22 15:30:12 +0400

    u

---
+++ InvokeDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Delegate;
+
+internal static class Program
+{
+    private delegate void MyDelegate(string text);
+
+    private static void Main()
+    {
+        MyDelegate myDelegate = new(Sing);
+        myDelegate += Dance;
+
+        myDelegate.Invoke("I am ");
+        myDelegate.Invoke("He is ");
+        myDelegate.Invoke("She is ");
+        myDelegate.Invoke("We are ");
+        myDelegate.Invoke("They are ");
+    }
+
+    #region Methods
+
+    private static void Sing(string text)
+    {
+        Console.WriteLine(text + "singing");
+    }
+
+    private static void Dance(string text)
+    {
+        Console.WriteLine(text + "dancing");
+    }
+
+    #endregion Methods
+}
