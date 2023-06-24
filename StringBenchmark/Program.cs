b10e740b61adce504a692857a9f7023ec75909d9
Mark <markhuna@gmail.com>
2023-06-24 20:16:10 +0400

    u

---
+++ StringBenchmark/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+const int max = 100000;
+StringConcat(); // 83s
+StringConcatMethod(); // 83s
+StringBuilderAppend(); //00s
+
+static void StringConcat()
+{
+    Console.WriteLine($"String+String {max} times");
+
+    Stopwatch stopwatch = new();
+    string text = "s";
+    stopwatch.Start();
+    for (int i = 0; i < max; i++) text += i;
+    stopwatch.Stop();
+
+    Console.WriteLine($"Time: {stopwatch.Elapsed}\r\n");
+}
+static void StringConcatMethod()
+{
+    Console.WriteLine($"String.Concat() {max} times");
+
+    Stopwatch stopwatch = new();
+    string text = "s";
+    stopwatch.Start();
+    for (int i = 0; i < max; i++) text = string.Concat(text, i);
+    stopwatch.Stop();
+
+    Console.WriteLine($"Time: {stopwatch.Elapsed}\r\n");
+}
+static void StringBuilderAppend()
+{
+    Console.WriteLine($"StringBuilder.Append() {max} times");
+
+    Stopwatch stopwatch = new();
+    StringBuilder text = new("b");
+    stopwatch.Start();
+    for (int i = 0; i < max; i++) text.Append(i);
+    stopwatch.Stop();
+
+    Console.WriteLine($"Time: {stopwatch.Elapsed}\r\n");
+}
